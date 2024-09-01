2645bf0c2c440802ee16f2d4ae0742229cf3f817
Abhishek090104 <160103082+Abhishek090104@users.noreply.github.com>
2024-09-01 19:12:10 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,9 +2,13 @@ import admin from 'firebase-admin';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+// Retrieve project ID from environment variables
+const projectId = process.env.FIREBASE_PROJECT_ID;
+
 // Read and parse the service account key JSON from environment variables
 const serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_JSON, 'base64').toString('utf8'));
 
+// Firebase configuration using the service account and project ID
 const firebaseConfig = {
   credential: admin.credential.cert(serviceAccount),
   databaseURL: `https://${projectId}.firebaseio.com`,
